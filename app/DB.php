28a644449b902e91a93f75c75ec864e1393f0103
Yurii Svystun <svystun@gmail.com>
2017-07-29 17:58:18 +0300

    Feature: Added DB and Model functionality.

--- app/DB.php
+++ app/DB.php
@@ -11,14 +11,23 @@ abstract class DB
     /**
      * @var null
      */
-    private static $instance = null;
-    private static $pdo = null;
+    protected static $instance = null;
+
+    /**
+     * @var null
+     */
+    protected $db = null;
+
+    /**
+     * @var null
+     */
+    protected $table = null;
 
     /**
      * @var array
      */
-    private static $db = [
-        'name' => 'bgcheck',
+    private static $conf = [
+        'name' => 'demo',
         'host' => '127.0.0.1',
         'user' => 'root',
         'pass' => 'root'
@@ -27,12 +36,14 @@ abstract class DB
     /**
      * Connection to DB
      *
-     * @return PDO|null
+     * @return null|static
      */
     public static function connect() {
 
-        if (self::$instance == null) {
-            self::$instance = new PDO('mysql:host='.self::$db['host'].';dbname='.self::$db['name'], self::$db['user'], self::$db['pass']);
+        if (is_null(self::$instance)) {
+            $instance = new static();
+            $instance->setDB();
+            self::$instance = $instance;
         }
         return self::$instance;
     }
@@ -52,11 +63,59 @@ abstract class DB
      */
     private function __construct() {}
 
-    public function all()
+    /**
+     * Set database connection
+     */
+    public function setDB()
     {
-        $sth = self::$instance->prepare("SELECT name, colour FROM fruit");
+        if (is_null($this->db)) {
+            $pdo = new PDO('mysql:host='.self::$conf['host'].';dbname='.self::$conf['name'], self::$conf['user'], self::$conf['pass']);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db = $pdo;
+        }
+    }
+
+    /**
+     * Get all data from table
+     *
+     * @param int $type
+     * @return mixed
+     */
+    public function all($type = PDO::FETCH_OBJ)
+    {
+        $sth = $this->db->prepare(sprintf("SELECT * FROM %s", $this->table));
         $sth->execute();
 
-        return $sth->fetchAll();
+        return $sth->fetchAll($type);
+    }
+
+    /**
+     * Execute SELECT query
+     *
+     * @param string $sql
+     * @param int $type
+     * @return mixed
+     */
+    public function query($sql = '', $type = PDO::FETCH_OBJ)
+    {
+        $sth = $this->db->prepare($sql);
+        $sth->execute();
+
+        return $sth->fetchAll($type);
+    }
+
+    /**
+     * Find by id
+     *
+     * @param int $id
+     * @param int $type
+     * @return mixed
+     */
+    public function find(int $id = 0, $type = PDO::FETCH_OBJ)
+    {
+        $sth = $this->db->prepare(sprintf("SELECT * FROM %s WHERE id = :id", $this->table));
+        $sth->execute(['id' => $id]);
+
+        return $sth->fetch($type);
     }
 }
